13b8d4cc52440fd5c8e836368de1af24ca53f718
gabduart <gabduart68@gmail.com>
2025-03-16 00:48:45 -0300

    Environment configured, Classes and database created

---
+++ WebApi8/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi8.Models;
+
+namespace WebApi8.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        // Criação da tabela autores na database com o modelo de AutorModel
+        public DbSet<AutorModel> Autores { get; set; }
+
+        // Criação da tabela livros na database com o modelo de LivroModel
+        public DbSet<LivroModel> Livros { get; set; }
+    }
+}
