459286c4ca9dd948d7eccc4b2c784ece9dcff3a2
gabduart <gabduart68@gmail.com>
2025-03-16 14:25:46 -0300

    Get route

--- WebApi8/obj/Debug/net8.0/WebApi8.AssemblyInfo.cs
+++ WebApi8/obj/Debug/net8.0/WebApi8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b8d4cc52440fd5c8e836368de1af24ca53f718")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
