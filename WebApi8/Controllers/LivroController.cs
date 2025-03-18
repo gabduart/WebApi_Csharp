4cda574b7cc7dfe2c98304c6d66ad72b6516b7c1
gabduart <gabduart68@gmail.com>
2025-03-18 09:52:57 -0300

    Get route, 'ListarLivros' done

---
+++ WebApi8/Controllers/LivroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi8.Models;
+using WebApi8.Services.Livro;
+
+namespace WebApi8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly ILivroInterface _livroInterface;
+        public LivroController(ILivroInterface livroInterface)
+        {
+            _livroInterface = livroInterface;
+        }
+
+        [HttpGet("ListarLivros")]
+        public async Task<ActionResult<ResponseModel<List<LivroModel>>>> ListarLivros()
+        {
+            var livros = await _livroInterface.ListarLivros();
+            return Ok(livros);
+        }
+    }
+}
---
+++ WebApi8/Services/Livro/LivroService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi8.Data;
+using WebApi8.Dto.Livro;
+using WebApi8.Models;
+
+namespace WebApi8.Services.Livro
+{
+    public class LivroService : ILivroInterface
+    {
+        private readonly AppDbContext _context;
+
+        public LivroService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ResponseModel<LivroModel>> BuscarLivroPorId(int idLivro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<LivroModel>> BuscarLivroPorIdAutor(int idAutor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<LivroModel>> CirarLivro(LivroCriacaoDto livroCriacaoDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<LivroModel>>> EditarLivro(LivroEdicaoDto livroEdicaoDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<LivroModel>>> ExcluirLivro(int idLivro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResponseModel<List<LivroModel>>> ListarLivros()
+        {
+            ResponseModel<List<LivroModel>> resposta = new ResponseModel<List<LivroModel>>();
+
+            try
+            {
+                var livros = await _context.Livros
+                    .Include(a => a.Autor)
+                    .ToListAsync();
+                resposta.Dados = livros;
+                resposta.Mensagem = "Todos os livros foram encontrados com sucesso!";
+                return resposta;
+            } catch (Exception ex)
+            {
+                resposta.Mensagem = "Nenhum livro foi encontrado!";
+                resposta.Status = false;
+                return resposta;
+            }
+        }
+    }
+}
